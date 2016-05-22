cad1b3c087f19a84a3f08b1397920f8b4b281162
lite3 <lite3@qq.com>
2016-05-22 23:15:45 +0800

    add SaveToAlbum for Android

--- Assets/CrossPlatformAPI/AndroidPlatform.cs
+++ Assets/CrossPlatformAPI/AndroidPlatform.cs
@@ -9,10 +9,16 @@ namespace litefeel
 {
     public class AndroidPlatform : PlatformAPI
     {
+        private AndroidJavaClass api = null;
+
+        public AndroidPlatform()
+        {
+            api = new AndroidJavaClass("litefeel.crossplatformapi.AndroidPlatform");
+        }
 
         public override void SaveToAlbum(string filename)
         {
-
+            api.CallStatic("saveToAlbum", new string[] { filename });
         }
     }
 }
--- Assets/CrossPlatformAPI/Examples/Test.cs
+++ Assets/CrossPlatformAPI/Examples/Test.cs
@@ -1,22 +1,38 @@
 using UnityEngine;
 using System.Collections;
 using litefeel;
+using System.IO;
 
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
         print(Application.platform);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        print("streamingAssetsPath:" + Application.streamingAssetsPath);
+        print("persistentDataPath:" + Application.persistentDataPath);
+
+        StartCoroutine(MoveToPersistentPath());
+        MoveToPersistentPath();
+    }
+
+    // Update is called once per frame
+    IEnumerator MoveToPersistentPath () {
+        string filename = Application.streamingAssetsPath + "/abc.png";
+        WWW www = new WWW(filename);
+        yield return www;
+        if (www.error != null)
+        {
+            print("can not load file form " + filename);
+        } else
+        {
+            File.WriteAllBytes(Application.persistentDataPath + "/abc.png", www.bytes);
+            www.Dispose();
+        }
+    }
 
     public void SavaToAlbum()
     {
-        string filename = Application.streamingAssetsPath + "/abc.png";
+        string filename = Application.persistentDataPath + "/abc.png";
         CrossPlatformAPI.SaveToAlbum(filename);
     }
 }
