b027bcc78c12aac6d5ba98ffb782259f7d24830d
litefeel <lite3@qq.com>
2017-07-26 09:28:44 +0800

    remove deprecated method

--- Assets/CrossPlatformAPI/CrossPlatformAPI.cs
+++ Assets/CrossPlatformAPI/CrossPlatformAPI.cs
@@ -4,12 +4,8 @@ using System;
 
 namespace litefeel.crossplatformapi
 {
-    /// <summary>
-    /// this class is deprecated.
-    /// @deprecated Please use specific class.
-    /// @see Album Share Clipboard
-    /// </summary>
-    public static class CrossPlatformAPI
+
+    public class CrossPlatformAPI : MonoBehaviour
     {
 
         /// <summary>
@@ -17,51 +13,23 @@ namespace litefeel.crossplatformapi
         /// </summary>
         public const string version = "1.0";
 
-        /// <summary>
-        /// @deprecated Please use <see cref="Album.SaveImage"/> instead. 
-        /// </summary>
-        [Obsolete("Method SaveToAlbum has been deprecated.Please use Album.SaveImage instead.")]
-        public static void SaveToAlbum(string imagePath)
+
+        private static CrossPlatformAPI instance = null;
+
+        internal static void Init()
         {
-            Album.SaveImage(imagePath);
+            if (instance == null)
+            {
+                var go = new GameObject("CrossPlatformAPI");
+                instance = go.AddComponent<CrossPlatformAPI>();
+                GameObject.DontDestroyOnLoad(go);
+            }
         }
 
-        /// <summary>
-        /// @deprecated Please use <see cref="Clipboard.SetText"/> instead. 
-        /// </summary>
-        [Obsolete("Method SetToClipboard has been deprecated.Please use Clipboard.SetText instead.")]
-        public static void SetToClipboard(string text)
+        private void OnUI_AlertCb(string message)
         {
-            Clipboard.SetText(text);
+            UIApi.OnAlertCb(message);
         }
-
-        /// <summary>
-        /// @deprecated Please use <see cref="Clipboard.GetText"/> instead. 
-        /// </summary>
-        [Obsolete("Method GetFromClipboard has been deprecated.Please use Clipboard.GetText instead.")]
-        public static string GetFromClipboard()
-        {
-            return Clipboard.GetText();
-        }
-
-        /// <summary>
-        /// @deprecated Please use <see cref="Share.ShareImage"/> instead. 
-        /// </summary>
-        [Obsolete("Method ShareImage has been deprecated.Please use Share.ShareImage instead.")]
-        public static void ShareImage(string imagePath, string text = null)
-        {
-            Share.ShareImage(imagePath, text);
-        }
-
-        /// <summary>
-        /// @deprecated Please use <see cref="Share.ShareText"/> instead. 
-        /// </summary>
-        [Obsolete("Method ShareText has been deprecated.Please use Share.ShareText instead.")]
-        public static void ShareText(string text)
-        {
-            Share.ShareText(text);
-        }
-
     }
 
 }
